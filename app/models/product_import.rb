16d051379af0ad3d093d866ac8f65851791cacc6
James Wigglesworth <wigsgiw@gmail.com>
2011-02-06 04:58:37 +1300

    fixing up heroku, using the tmp folder to store the uploaded files. remember they may be around for whole request, may not. so possibly flaky.

--- app/models/product_import.rb
+++ app/models/product_import.rb
@@ -5,7 +5,7 @@
 # License:: MIT
 
 class ProductImport < ActiveRecord::Base
-  has_attached_file :data_file, :path => ":rails_root/lib/etc/product_data/data-files/:basename.:extension"
+  has_attached_file :data_file, :path => ":rails_root/tmp/product_data/data-files/:basename.:extension"
   validates_attachment_presence :data_file
   
   require 'fastercsv'
